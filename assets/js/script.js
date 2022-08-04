cca497feda2c144fcbd15a7aef2592de2b725cb4
Zé Cláudio <zeclaudio_jr5@yahoo.com.br>
2022-08-04 15:24:12 -0300

    Adição da data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ const segundos = document.getElementById('segundos');
 
 
 const relogio = setInterval(function time() {
-    let dateToday = new Date();
-    let hr = dateToday.getHours();
-    let min = dateToday.getMinutes();
-    let s = dateToday.getSeconds();
+    let horarioToday = new Date();
+    let hr = horarioToday.getHours();
+    let min = horarioToday.getMinutes();
+    let s = horarioToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
@@ -16,4 +16,28 @@ const relogio = setInterval(function time() {
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+})
+
+
+
+
+
+const dia = document.getElementById('dia');
+const mes = document.getElementById('mes');
+const ano = document.getElementById('ano');
+
+
+const calendario = setInterval(function calendar() {
+    let dateToday = new Date();
+    let d = dateToday.getDay();
+    let m = dateToday.getMonth();
+    let a = dateToday.getFullYear();
+
+    if (d < 10) d = '0' + d;
+    if (m < 10) m = '0' + m;
+    if (a < 10) a = '0' + a;
+
+    dia.textContent = d;
+    mes.textContent = m;
+    ano.textContent = a;
 })
