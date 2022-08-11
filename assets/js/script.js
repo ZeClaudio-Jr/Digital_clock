df054f7874662cce33d921f5d9cfb85f29cbc7ba
Zé Cláudio <zeclaudio_jr5@yahoo.com.br>
2022-08-11 00:40:22 -0300

    Consertos do mês e adição de informação geral

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ const relogio = setInterval(function time() {
     let min = horarioToday.getMinutes();
     let s = horarioToday.getSeconds();
 
+
+
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (s < 10) s = '0' + s;
@@ -25,13 +27,15 @@ const relogio = setInterval(function time() {
 const dia = document.getElementById('dia');
 const mes = document.getElementById('mes');
 const ano = document.getElementById('ano');
+const infoG = document.getElementById('infoG');
 
 
-const calendario = setInterval(function calendar() {
+const calendario = setInterval(function time() {
     let dateToday = new Date();
-    let d = dateToday.getDay();
-    let m = dateToday.getMonth();
+    let d = dateToday.getDate();   // O getDay não funciona como eu pensava.
+    let m = dateToday.getMonth()+1;   // Só o getMonth trás o mês anterior ao que se está. O método getMonth() retorna o mês da data especificada. É importante observar a respeito do método getMonth() que ele retorna valores indexados em zero (ou seja, de 0 a 11), onde 0 representa janeiro e 11 representa dezembro. Por isso, é preciso adicionar 1 para "normalizar" o valor do mês.
     let a = dateToday.getFullYear();
+    let i = dateToday.toUTCString();
 
     if (d < 10) d = '0' + d;
     if (m < 10) m = '0' + m;
@@ -40,4 +44,5 @@ const calendario = setInterval(function calendar() {
     dia.textContent = d;
     mes.textContent = m;
     ano.textContent = a;
+    infoG.textContent = i;
 })
