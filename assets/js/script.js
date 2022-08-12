94bacb2295aa4903d94efe921c224617e3e35241
Zé Cláudio <zeclaudio_jr5@yahoo.com.br>
2022-08-12 15:16:34 -0300

    Melhoramento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Código javascript para formação do relógio
+
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
@@ -22,33 +24,33 @@ const relogio = setInterval(function time() {
 
 
 
-
+// Código javascript para formação do calendário
 
 const dia = document.getElementById('dia');
 const mes = document.getElementById('mes');
 const ano = document.getElementById('ano');
 
-const infoG = document.getElementById('infoG');
+const infoG = document.getElementById('infoG'); // Implementação do horário mundial, dizendo que a constante infoG (primeira da linha e criada agora) vai representar o Id=infoG do código HTML
 
 
 const calendario = setInterval(function time() {
     let dateToday = new Date();
     let d = dateToday.getDate();   // O getDay não funciona como eu pensava.
     // let d = dateToday.getDay();  //Não funcionou!
-    let m = dateToday.getMonth()+1;   // Só o getMonth trás o mês anterior ao que se está. O método getMonth() retorna o mês da data especificada. É importante observar a respeito do método getMonth() que ele retorna valores indexados em zero (ou seja, de 0 a 11), onde 0 representa janeiro e 11 representa dezembro. Por isso, é preciso adicionar 1 para "normalizar" o valor do mês.
+    let m = dateToday.getMonth()+1;   // O "getMonth" sozinho trás o mês anterior ao que se está. O método getMonth() retorna o mês da data especificada. É importante observar a respeito do método getMonth() que ele retorna valores indexados em zero (ou seja, de 0 a 11), onde 0 representa janeiro e 11 representa dezembro. Por isso, é preciso adicionar 1 para "normalizar" o valor do mês.
     let a = dateToday.getFullYear();
 
-    let i = dateToday.toUTCString();
+    let i = dateToday.toUTCString();  // Informação de em que o "i" se transformará
 
 
-    
     if (d < 10) d = '0' + d;
     if (m < 10) m = '0' + m;
     if (a < 10) a = '0' + a;
+
 
     dia.textContent = d;
     mes.textContent = m;
     ano.textContent = a;
 
-    infoG.textContent = i;
+    infoG.textContent = i; // Transformação do infoG em "i"
 })
